a0a7a53fa9e76dacd2c180cf51604dfc6c00e0c6
Jiekun <jiekun.dev@gmail.com>
2021-08-02 18:55:36 +0800

    Fixed charset config

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,14 +1,14 @@
 create database entry_task_v2_db;
 
-CREATE TABLE `user_tab` (
-    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'username',
-    `nickname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'nickname',
-    `profile_pic` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'user avatar url',
-    `password` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'user password (encrypted)',
-    `status` tinyint unsigned NOT NULL COMMENT 'user status 0-enabled 1-disabled',
-    `ctime` int unsigned NOT NULL COMMENT 'create timestamp',
-    `mtime` int unsigned NOT NULL COMMENT 'modify timestamp',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uniq_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+create table `user_tab` (
+    `id` bigint unsigned not null auto_increment,
+    `name` varchar(64) collate utf8mb4_unicode_ci not null comment 'username',
+    `nickname` varchar(64) collate utf8mb4_unicode_ci not null comment 'nickname',
+    `profile_pic` varchar(1024) collate utf8mb4_unicode_ci not null comment 'user avatar url',
+    `password` varchar(128) collate utf8mb4_unicode_ci not null comment 'user password (encrypted)',
+    `status` tinyint unsigned not null comment 'user status 0-enabled 1-disabled',
+    `ctime` int unsigned not null comment 'create timestamp',
+    `mtime` int unsigned not null comment 'modify timestamp',
+    primary key (`id`),
+    unique key `uniq_name` (`name`)
+) engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
