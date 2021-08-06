e298add8cb73c9671c787fc76f434d09aaa41058
Jiekun <jiekun.dev@gmail.com>
2021-08-06 19:35:16 +0800

    Added test cases and mock package

---
+++ internal/erpc-service/user_test.go
@@ -0,0 +1,60 @@
+// @Author: 2014BDuck
+// @Date: 2021/8/6
+
+package erpc_service
+
+import (
+	"context"
+	"github.com/2014bduck/entry-task/internal/dao"
+	"github.com/2014bduck/entry-task/internal/models"
+	"github.com/2014bduck/entry-task/pkg/hashing"
+	erpc_proto "github.com/2014bduck/entry-task/proto/erpc-proto"
+	"github.com/agiledragon/gomonkey"
+	"gorm.io/gorm"
+	"reflect"
+	"testing"
+)
+
+func TestUserService_Register(t *testing.T) {
+	svc := NewUserService(context.Background())
+
+	// Mock stuffs
+	username := "test_username"
+	nickname := "test_nickname"
+	password := "test_password"
+
+	// Input
+	request := erpc_proto.RegisterRequest{
+		Username: username,
+		Nickname: nickname,
+		Password: password,
+	}
+	// Target output
+	want := &erpc_proto.RegisterReply{}
+
+	// Mock all DAO call
+	patches := gomonkey.ApplyMethod(reflect.TypeOf(svc.dao), "GetUserByName", func(_ *dao.Dao, _ string) (models.UserTab, error) {
+		return models.UserTab{}, gorm.ErrRecordNotFound
+	})
+	defer patches.Reset()
+	patches.ApplyFunc(hashing.HashPassword, func(_ string) string {
+		return "mock_hashing"
+	})
+	patches.ApplyMethod(reflect.TypeOf(svc.dao), "CreateUser", func(_ *dao.Dao, _, _, _, _ string, _ uint8) (*models.UserTab, error) {
+		return &models.UserTab{
+			Name:     username,
+			Nickname: nickname,
+			Password: password, // It's hashed actually.
+		}, nil
+	})
+
+	// Test and compare with reflect.DeepEqual
+	resp, err := svc.Register(request)
+	if err != nil {
+		t.Errorf("TestUserService_Register got error %v", err)
+	}
+
+	if !reflect.DeepEqual(want, resp) {
+		t.Errorf("TestUserService_Register want: %v got %v", want, resp)
+	}
+}
